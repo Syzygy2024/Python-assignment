005e6d57a4da338b042714607f8d9fc4a77ebaac
Syzygy2024 <160586177+Syzygy2024@users.noreply.github.com>
2024-03-04 22:01:41 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Get user input
+name = input("What is your name?")
+age = input("How old are you? ")
+location = input("Where do you live?")
+
+print(f"\nHello {name}! Nine to meet you.")
+print(f"You are {age} years old.")
+print(f"I see you are located in {location}.")
+print("Have a great day!")
+
+print(f"Hello {name},you are {age} years old and live in {location}.")
